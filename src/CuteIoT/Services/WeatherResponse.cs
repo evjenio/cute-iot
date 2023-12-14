1cc06d36a8eb7cde72be73540dbc7765f3ebf2b1
Evgeny Drenkov <evjenio@ya.ru>
2023-12-14 21:39:42 +0300

    Removed clockWidget

--- src/CuteIoT/Services/WeatherResponse.cs
+++ src/CuteIoT/Services/WeatherResponse.cs
@@ -28,6 +28,7 @@ namespace CuteIoT.Services
     {
         public string Main { get; set; }
         public string Description { get; set; }
+        public string Icon { get; set; }
     }
 
     //   {
